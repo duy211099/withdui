7bdec53a382736d206f7627aa0f876b42f991ece
duy <duy211099@gmail.com>
2025-11-02 23:14:32 +0700

    feat: inertia Dynamic sharing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # Dynamic sharing: Data is evaluated at render time
+  # inertia_share do
+  #   {
+  #     user: current_user,
+  #     notifications: current_user&.unread_notifications_count
+  #   } if user_signed_in?
+  # end
 end

4c1997e188ffdd297326c5c1e4407ed50c0191bc
duy <duy211099@gmail.com>
2026-01-05 02:17:58 +0700

    revide: ruby methods

--- ruby/flicks/flicks.rb
+++ ruby/flicks/flicks.rb
@@ -1,8 +1,33 @@
-puts "Ruby is friendly!"
+# puts "Ruby is friendly!"
 
-phrase = "I am sleepy"
-puts phrase
+# phrase = "I am sleepy"
+# puts phrase
 
-city = "Dalat"
-rank = "the best"
-puts "I'm in #{city}\nMy hometown is #{rank}!"
+# city = "Dalat"
+# rank = "the best"
+# puts "I'm in #{city}\nMy hometown is #{rank}!"
+
+def emojis(emoji, number)
+  emoji * number
+end
+
+def movie_info(title = "None", rank = 1)
+  stars = emojis("⭐️", rank)
+  "#{title} has a rank of #{stars}"
+end
+
+def movie_info_2(title: "Hi", rank: 9)
+  stars = emojis("🌝", rank)
+  "#{title} has a rank of #{stars}"
+end
+
+movie="dui"
+rank=3
+
+puts movie_info
+puts movie_info("Goonies", 10)
+puts movie_info(movie, rank)
+
+puts movie_info_2
+puts movie_info_2(title: "Goonies", rank: 10)
+puts movie_info_2(rank: rank, title: movie)
---
+++ ruby/flicks/note.rb
@@ -0,0 +1,31 @@
+# ____ NOTE ____
+
+# 1. Strings
+
+puts "Ruby is friendly!"
+
+phrase = "I am sleepy"
+puts phrase
+
+city = "Dalat"
+rank = "the best"
+puts "I'm in #{city}\nMy hometown is #{rank}!"
+
+
+# 2. Objects Everywhere
+
+# title = "WithDui"
+# title.length
+# # title
+# title.upcase
+# title.upcase!
+# # title
+# title.capitalize!
+# title.empty?
+# title.center(10)
+# _wow = rank * 2 == rank.*(2)
+
+# _______________________________
+
+# 3. Self Curiosity
+# self.puts === puts

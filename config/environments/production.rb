0cf1e106681d89be4f37b9adda641a91e85f32f8
duy <duy211099@gmail.com>
2026-01-07 00:19:50 +0700

    Revert "config: subdomain"
    This reverts commit 1d5799e472807013214255579a6d67d5e3f80c26.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,6 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
-  config.hosts << ".withdui.dev"
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [

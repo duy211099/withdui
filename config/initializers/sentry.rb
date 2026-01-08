1131f9e32ea3cabd6b5ae6d74007ceb9118410a6
duy <duy211099@gmail.com>
2026-01-09 00:25:13 +0700

    chore: config sentry for testing

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,8 +3,11 @@ unless Rails.env.test?
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
     config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
-    config.traces_sample_rate = 0.1
-    config.profiles_sample_rate = 0.1
+    config.traces_sample_rate = 1.0
+    config.profiles_sample_rate = 1.0
     config.send_default_pii = true
+    config.debug = true
+    config.logger = Logger.new($stdout)
+    config.logger.level = Logger::DEBUG
   end
 end
